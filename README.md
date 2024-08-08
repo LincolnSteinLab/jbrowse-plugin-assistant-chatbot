9134c448f9d6fec7f4f74c506f7393de76f793a9
Caroline Bridge <cbridge@oicr.on.ca>
2024-08-08 16:05:51 -0400

    initial bot scaffold

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # jb-bot
+
+A tool for querying your JBrowse configuration file.
+
+## Installation
+
+### Prerequisites
+
+### Installation steps
+
+## Usage
+
+### Interactively
+
+```
+poetry run python bin/jb-bot.py -c my-config.json
+```
+
+### Non-interactively
+
+```
+poetry run python bin/jb-bot.py -p "What tracks do you have that show epigenetic modifications?" -c my-config.json
+```
+
+## Options
+
+```
+-c -config : your JBrowse configuration file.
+-h -help   : print this help message and exit.
+-p -prompt : prompt query regarding your JBrowse config.json.
+```
+
+## Configuration
+
+### Environment variables
+
+- `GOOGLE_API_KEY`: API key to interface with [Google Gemini]() (required).
+
+Configure your environment variables with the `POETRY_` alias:
+
+```
+export POETRY_GOOGLE_API_KEY=your_google_api_key
+```
+
+**OR** place the following in your `.env` file:
+
+```
+GOOGLE_API_KEY=your_google_api_key
+```
+
+and install the poetry plugin `poetry-plugin-dotenv`:
+
+```
+pipx inject poetry poetry-plugin-dotenv
+```
+
+## Web UI
+
+## Acknowledgements
+
+The author(s) of this project recognize the assistance and inspiration from the authors of [Reactome ChatBot](https://github.com/reactome/reactome_chatbot).
---
+++ jb-bot/app.py
@@ -0,0 +1,40 @@
+import argparse
+
+from ChatBot import ChatBot
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument('-c', '--config', help='path to a JBrowse configuration file you would like to query.', required=True)
+    parser.add_argument('-p', '--prompt', type=str, help='prompt query regarding your JBrowse config.json', required=False)
+    parser.add_argument('-t', '--host', help='host URL for your JBrowse session', required=False)
+
+    args = parser.parse_args()
+
+    print('Initializing ChatBot...')
+
+    if args.prompt:
+        llm = ChatBot(args.config, args.host, False)
+        non_interactive(llm, args.prompt)
+    else:
+        llm = ChatBot(args.config, args.host, True)
+        interactive(llm)
+    
+    
+
+def non_interactive(llm, prompt):
+    print(f'\nResponse: {llm.run(prompt)}')
+
+def interactive(llm):
+    open_chat = True
+    print('This interactive JBrowse assistant can help you navigate your configuration file. Enter a query relating to your config.json, or enter "q" to exit.')
+    while open_chat is True:
+        query = input('\nQuery: ')
+        if (query.lower().rstrip() == 'q'):
+            open_chat = False
+        else:
+            print(f'\nResponse: \n\n{llm.run(query)}')
+
+
+if __name__ == "__main__":
+    main()
