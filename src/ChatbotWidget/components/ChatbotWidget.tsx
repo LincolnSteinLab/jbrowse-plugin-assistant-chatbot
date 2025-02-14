e2011dc840840417bcaba71f9a9227f40a89b7e4
Greg Hogue <gfjhogue@gmail.com>
2025-02-13 20:26:54 -0500

    simple OpenAI chat widget

--- src/ChatbotWidget/components/ChatbotWidget.tsx
+++ src/ChatbotWidget/components/ChatbotWidget.tsx
@@ -1,24 +1,84 @@
-import { ChatModelAdapter, Thread, useLocalRuntime } from '@assistant-ui/react'
+import {
+  AssistantRuntimeProvider,
+  ChatModelAdapter,
+  ChatModelRunOptions,
+  Thread,
+  ThreadAssistantContentPart,
+  ThreadMessage,
+  useLocalRuntime,
+} from '@assistant-ui/react'
+import { AIMessage, BaseMessage, BaseMessageFields, HumanMessage, SystemMessage } from '@langchain/core/messages';
 import React from 'react'
 import { createGlobalStyle } from 'styled-components';
-import styles from '@assistant-ui/react/styles/index.css';
+import { ChatAgent, getMessageContentText } from '../../chat/ChatAgent';
 
-const LocalLangchainAdapter: ChatModelAdapter = {
-  async *run({ messages, abortSignal, config }) {
-    const stream = '...'
+import auiStyles from '@assistant-ui/react/styles/index.css';
+import customStyles from './ChatbotWidget.css'
+
+const AssistantUIStyle = createGlobalStyle`${auiStyles}`
+const CustomStyle = createGlobalStyle`${customStyles}`
+
+const chatAgent = new ChatAgent();
+
+class LocalLangchainAdapter implements ChatModelAdapter {
+  async *run({
+    messages,
+    runConfig,
+    abortSignal,
+    context,
+  }: ChatModelRunOptions) {
+    const lc_messages: BaseMessage[] = messages.map((tm: ThreadMessage) => {
+      const {
+        id, createdAt, role, content, metadata, attachments, status
+      } = tm;
+      let fields: BaseMessageFields = {
+        content: tm.content.map(part => {
+          switch (part.type) {
+            case 'text':
+              return { type: 'text', text: part.text };
+            default:
+              throw new Error(`Unknown content part type: ${part.type}`);
+          }
+        }),
+        id: tm.id,
+      };
+      switch (tm.role) {
+        case 'system':
+          return new SystemMessage(fields);
+        case 'assistant':
+          return new AIMessage(fields);
+        case 'user':
+          return new HumanMessage(fields);
+        default:
+          throw new Error(`Unknown message role: ${role}`);
+      }
+    });
+    const stream = chatAgent.stream(lc_messages);
+    let text = '';
+    for await (const part of stream) {
+      text += getMessageContentText(part);
+      yield {
+        content: [{ type: 'text', text }] as ThreadAssistantContentPart[],
+      };
+    }
+    return;
   }
 }
 
-const AssistantUIStyle = createGlobalStyle`${styles}`
-
 export default function ReactComponent() {
-  const runtime = useLocalRuntime(LocalLangchainAdapter)
+  const adapter = new LocalLangchainAdapter()
+  const runtime = useLocalRuntime(adapter)
   return (
-    <div>
+    <AssistantRuntimeProvider runtime={runtime}>
       <AssistantUIStyle />
-      <div className='h-full'>
-        <Thread runtime={runtime} />
+      <CustomStyle />
+      <div className='chat-widget'>
+        <Thread
+          welcome={{
+            message: 'Enter your OpenAI API key to start chatting.',
+          }}
+        />
       </div>
-    </div>
+    </AssistantRuntimeProvider>
   )
 }
